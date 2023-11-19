23eac28a11d6da0107b292686a054edb7d4078fe
Crowdbotics <team@crowdbotics.com>
2023-11-19 19:35:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "infotox-44672",
+  url: "https://infotox-44672.botics.co",
+  api: "https://infotox-44672.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
